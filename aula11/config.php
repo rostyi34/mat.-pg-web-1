6b955055f792f2cac62600a565207319fa2ec03e
gustavo <gustavo.rossetti@unidavi.edu.br>
2025-11-25 22:05:52 -0300

    dsadsa

---
+++ aula11/config.php
@@ -0,0 +1,11 @@
+<?php
+// config.php
+// Ajuste as constantes abaixo conforme seu ambiente
+define('DB_HOST', 'localhost');
+define('DB_PORT', '5432');
+define('DB_NAME', 'avaliacao');
+define('DB_USER', 'postgres');
+define('DB_PASS', '1234');
+
+// Charset / options
+define('DB_DSN', 'pgsql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';');
